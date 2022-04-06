688cc62679ee47a896b4175b452fcbade9bd6e44
abbosxon <abbosxon.oripov@yandex.ru>
2022-04-06 15:13:21 +0500

    add answers in html

--- js/main.js
+++ js/main.js
@@ -1,30 +1,42 @@
 let son1 = prompt("1-chi sonni kiriting:")
 let a = prompt("ishorani belgilang: + * - / %")
 let son2 = prompt("2-chi sonni kiriting:")
+let html = document.getElementById('test;')
 console.log(isNaN(son1), isNaN(son2));
 if (son1 && son2) {
     if (isNaN(son1) || isNaN(son2)) {
         console.log("Raqamni kiriting , boshqa belgini emas !");
         if (isNaN(son1)) {
+            test.innerHTML = ("1-chi kiritilgan belgi son emas");
             console.log("1-chi kiritilgan belgi son emas");
         }
         if (isNaN(son2)) {
+            test.innerHTML = ("2-chi kiritilgan belgi son emas");
             console.log("2-chi kiritilgan belgi son emas");
         }
 
     } else if (a == '+') {
+        test.innerHTML = ("Javob =", Number(son1) + Number(son2));
         console.log("Javob =", Number(son1) + Number(son2));
     } else if (a == '-') {
+        test.innerHTML = ("Javob =", Number(son1) - Number(son2));
         console.log("Javob =", Number(son1) - Number(son2));
     } else if (a == '*') {
+        test.innerHTML = ("Javob =", Number(son1) * Number(son2));
         console.log("Javob =", Number(son1) * Number(son2));
     } else if (a == '/') {
+        test.innerHTML = ("Javob =", Number(son1) / Number(son2));
+
         console.log("Javob =", Number(son1) / Number(son2));
     } else if (a == '%') {
+        test.innerHTML = ("Javob =", Number(son1) % Number(son2));
         console.log("Javob =", Number(son1) % Number(son2));
     } else {
+        test.innerHTML = ("ishora notog'ri berilgan");
+
         console.log("ishora notog'ri berilgan");
     }
 } else {
+    test.innerHTML == ("sonni kiritmadingiz");
     console.log("sonni kiritmadingiz");
 }
